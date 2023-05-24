ade08dd6666b34f08f5c3d173f1f1ed29244ce00
YYBartT <bteunis@opera.com>
2023-05-24 11:40:30 +0200

    Docs Update * Login functions

--- docs/login.js
+++ docs/login.js
@@ -2,9 +2,10 @@
 
 /**
  * @function modio_user_is_logged
- * @desc This functions checks if the user is logged in.
+ * @desc This functions checks if the user is currently logged in.
  * 
- * [[NOTE: This function is local, it only checks whether access_token exists. If you want to confirm that you are logged in, you should try to call a function that requires a valid access_token.]]
+ * [[NOTE: This function is local, it only checks if an access token, returned by any of the functions in ${module.auth}, exists.
+ * If you want to confirm that you are logged in, you should try to call a function that requires a valid access token.]]
  * 
  * @returns {boolean} Whether the user is logged in or not
  * 
@@ -13,18 +14,18 @@
 
 /**
  * @function modio_user_access_token
- * @description This function returns the access_token
+ * @description This function returns the access token.
  * 
- * To confirm the access token is valid you show do a test request.
+ * To confirm that the access token is valid you should do a test request.
  * 
- * @returns {string} The access token
+ * @returns {string} The access token (an empty string `""` if it doesn't exist)
  * 
  * @function_end
  */
 
 /**
  * @function modio_user_login_from_cache
- * @description This function loads the access token saved locally from the last succeeded login.
+ * @description This function loads the access token that was saved locally from the last succeeded login.
  * 
  * @function_end
  */
