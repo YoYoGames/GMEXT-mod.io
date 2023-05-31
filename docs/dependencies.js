5a62222592d1c90b360a6b6b3a6cfa1fb2ec2c09
gurpreetsinghmatharoo <gurpreetsingh793@gmail.com>
2023-05-31 15:47:25 +0530

    dependencies done

--- docs/dependencies.js
+++ docs/dependencies.js
@@ -6,10 +6,10 @@
  * 
  * <br />
  * 
- * Get all dependencies the chosen mod has selected. This is useful if a mod requires other mods be installed for it to run. A successful request will return an array of ${struct.ModDependenciesObject}.
+ * This function gets all the dependencies the chosen mod has selected. This is useful if a mod requires other mods to be installed for it to run. A successful request will return an array of ${struct.ModDependenciesObject}.
  *
- * [[IMPORTANT: Because of the complexity of supporting nested dependencies, we recommend you treat dependencies as a recommendation for your players, and do not process dependencies automatically when installing a mod unless absolutely required. A successful request will return an array of ${struct.ModDependenciesObject}.]]
- *
+ * From the [mod.io docs](https://docs.mod.io/#get-mod-dependencies)""
+ * [[IMPORTANT: Because of the complexity of supporting nested dependencies, we recommend you treat dependencies as a recommendation for your players, and do not process dependencies automatically when installing a mod unless absolutely required.]]
  * [[NOTE: Some modders might select soft dependencies to promote or credit other mods. We advise against this but it is possible to do, and is one of the reasons why we recommend against processing nested dependencies automatically.]]
  * 
  * @param {string} mod_id Unique id of the mod.
@@ -27,10 +27,10 @@ function modio_dependencies_get() {}
  * 
  * <br />
  * 
- * Add mod dependencies required by the corresponding mod. A dependency is a mod that should be installed for this mod to run. A successful request returns a ${struct.Message}.
+ * This function adds mod dependencies required by the given mod. A dependency is a mod that should be installed for a mod to run. A successful request returns a ${struct.Message}.
  * 
  * @param {real} mod_id Unique id of the mod.
- * @param {string} dependencies One or more mod IDs that this mod is dependent on. Every dependency to add requires a separate field with dependencies[] as the key (eg. dependencies[]=1, dependencies[]=2). Max of 5 dependencies per request.
+ * @param {string} dependencies One or more mod IDs that this mod is dependent on. Every dependency to add requires a separate field with dependencies[] as the key (eg. dependencies[]=1, dependencies[]=2). Maximum of 5 dependencies per request.
  * 
  * @param {function|method} callback_success The function to trigger when the request is successful.
  * @param {function|method} callback_failed The function to trigger when the request failed.
@@ -45,7 +45,7 @@ function modio_dependencies_add() {}
  * 
  * <br />
  * 
- * Delete mod dependencies the corresponding mod has selected. A successful request will return `204 No Content`.
+ * This function deletes the given mod dependencies that the given mod has selected. A successful request will return `204 No Content`.
  * 
  * @param {real} mod_id Unique id of the mod.
  * @param {string} dependencies One or more mod IDs you want to delete as a dependency. Every dependency to delete requires a separate field with dependencies[] as the key (eg. dependencies[]=1, dependencies[]=2).
@@ -63,7 +63,7 @@ function modio_dependencies_delete() {}
  * 
  * @member {real} mod_id Unique ID of the mod that serves as the dependency.
  * @member {string} name Name of the mod dependency.
- * @member {string} name_id Path for the mod on mod.io. For example: https://mod.io/g/rogue-knight/m/**rogue-knight-hd-pack **
+ * @member {string} name_id Path for the mod on mod.io. For example: https://mod.io/g/rogue-knight/m/**rogue-knight-hd-pack**
  * @member {int64} date_added Unix timestamp of date the dependency was added.
  * @member {real} dependency_depth When a dependency depth is greater than zero (0), it means that the dependencies themselves rely on additional dependencies. To ensure smooth installation, it is recommended dependencies be installed in *descending* order of depth, beginning with those with the highest depth. Please note only dependencies with a depth of up to 5 will be shown.
  * @member {struct.Logo} logo Contains media URL's to the logo for the mod.
